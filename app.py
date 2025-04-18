effeb83d2224b0ee54eb6dafb09e562259ea3117
almoutasem alleji <112810886+almoutasema@users.noreply.github.com>
2025-04-18 14:10:10 +0200

    DB now working

--- app.py
+++ app.py
@@ -16,8 +16,13 @@ entry=tk.Entry(root, width=50)
 entry.pack(pady=10)
 
 
+def send_message():
+    if entry.get():
+        messages_col.insert_one({"text": entry.get()})
+        entry.delete(0, tk.END)
 
-sen_button = tk.Button(root, text="Send")
+
+sen_button = tk.Button(root, text="Send", command=send_message)
 sen_button.pack(pady=10)
 
 root.mainloop()
