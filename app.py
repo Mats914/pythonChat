669c1513aafd7935f4595307889d21fa476351ab
almoutasem alleji <112810886+almoutasema@users.noreply.github.com>
2025-04-18 13:48:48 +0200

    added gui

--- app.py
+++ app.py
@@ -3,3 +3,21 @@ import pymongo
 
 # Connect to MongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["pythonChat"]
+messages_col = db["messages"]
+
+
+# GUI
+root = tk.Tk()
+root.title("pythonChat")
+root.geometry("500x500") 
+
+entry=tk.Entry(root, width=50)
+entry.pack(pady=10)
+
+
+
+sen_button = tk.Button(root, text="Send")
+sen_button.pack(pady=10)
+
+root.mainloop()
