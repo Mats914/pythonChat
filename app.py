cf91998bdcb17473d28f31e7025b04f07c233d87
almoutasem alleji <112810886+almoutasema@users.noreply.github.com>
2025-04-18 16:02:09 +0200

    Finished product

--- app.py
+++ app.py
@@ -25,4 +25,15 @@ def send_message():
 sen_button = tk.Button(root, text="Send", command=send_message)
 sen_button.pack(pady=10)
 
+
+message_labbel=tk.Label(root, text="Messages:", justify="left")
+message_labbel.pack(pady=10)
+
+
+def fetch_messages():
+    messages = messages_col.find().sort("_id")
+    message_labbel.config(text="Messages:\n" + "\n".join( f"-{m["text"]}"for m in messages))
+    root.after(1000, fetch_messages)  # Fetch messages every second 
+
+fetch_messages()
 root.mainloop()
